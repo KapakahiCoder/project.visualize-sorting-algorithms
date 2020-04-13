3ea63179fb29c7552563f409984040771c8bf045
Kapakahi <rubbishman@gmail.com>
2020-04-13 21:30:20 +0900

    Completed tests and sort function...that's the easy part

--- src/Sort.js
+++ src/Sort.js
@@ -2,8 +2,18 @@ class SomeKindOfSort {
   constructor(array) {
     this.array = array;
   }
-  sort() {
-    console.log("sort!");
+
+  sort(inputArray) {
+    for (let i = 1; i < inputArray.length; i++) {
+      let key = inputArray[i];
+      let j = i - 1;
+      while (j >= 0 && inputArray[j] > key) {
+        inputArray[j + 1] = inputArray[j];
+        j = j - 1;
+      }
+      inputArray[j + 1] = key;
+    }
+    return inputArray;
   }
 
   returnValue(value) {
--- test/_sort.js
+++ test/_sort.js
@@ -1,11 +1,26 @@
 const Sort = require("../src/Sort");
 const { expect } = require("chai");
 
+let array;
+let test = new Sort(array);
+
 describe("Sort", () => {
   it("should be a function", () => {
     expect(Sort).to.be.a("function");
   });
   it("should have a sort method", () => {
     expect(Sort.prototype.sort).to.be.a("function");
+  });
+  it("should properly sort an array", () => {
+    expect(test.sort([3, 5, 10, 0, 4, 22])).to.eql([0, 3, 4, 5, 10, 22]);
+    expect(test.sort([-11, -3, 3, 0, -100, 4])).to.eql([
+      -100,
+      -11,
+      -3,
+      0,
+      3,
+      4,
+    ]);
+    expect(test.sort([1, 2, 3])).to.eql([1, 2, 3]);
   });
 });
